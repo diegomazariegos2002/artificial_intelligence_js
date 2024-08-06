4af5d2ee34d8d8483b589ecf6fc0f0484cb78abc
diegomazariegos2002 <3016552860101@ingenieria.usac.edu.gt>
2024-08-05 22:21:43 -0600

    feat(backend): se añade tarea 1 lógica

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,25 +1,33 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
+function reflex_agent(location, state) {
+    if (state == "DIRTY") return "CLEAN";
+    else if (location == "A") return "RIGHT";
+    else if (location == "B") return "LEFT";
 }
 
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";		
-	setTimeout(function(){ test(states); }, 2000);
+function test(states) {
+    var location = states[0];
+    var state = states[0] == "A" ? states[1] : states[2];
+    var action_result = reflex_agent(location, state);
+    document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" | State: ").concat(states);
+    
+    if (action_result == "CLEAN") {
+        if (location == "A") states[1] = "CLEAN";
+        else if (location == "B") states[2] = "CLEAN";
+    } else if (action_result == "RIGHT") states[0] = "B";
+    else if (action_result == "LEFT") states[0] = "A";
+
+    // Check if all states have been visited
+    if (states[1] == "CLEAN" && states[2] == "CLEAN" && location == "A") {
+        document.getElementById("log").innerHTML += "<br>All states visited!";
+        return;
+    }
+
+    setTimeout(function() { test(states); }, 2000);
 }
 
-var states = ["A","DIRTY","DIRTY"];
+// Start the simulation with initial state
+var states = ["A", "DIRTY", "DIRTY"];
 test(states);
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # artificial_intelligence_js
 
-[Reflex Agent](https://luisespino.github.io/artificial_intelligence_js/01_reflex_agent.html)
+[Reflex Agent](https://diegomazariegos2002.github.io/artificial_intelligence_js/01_reflex_agent.html)
 
-[Breadth and Depth Search](https://luisespino.github.io/artificial_intelligence_js/02_breadth_depth_search.html)
+[Breadth and Depth Search](https://diegomazariegos2002.github.io/artificial_intelligence_js/02_breadth_depth_search.html)
 
-[Uniform Cost Search](https://luisespino.github.io/artificial_intelligence_js/03_uniform_cost.html)
+[Uniform Cost Search](https://diegomazariegos2002.github.io/artificial_intelligence_js/03_uniform_cost.html)
 
-[Best First Search](https://luisespino.github.io/artificial_intelligence_js/04_bestfirst.html)
+[Best First Search](https://diegomazariegos2002.github.io/artificial_intelligence_js/04_bestfirst.html)
 
-[AStar Search](https://luisespino.github.io/artificial_intelligence_js/05_astar.html)
+[AStar Search](https://diegomazariegos2002.github.io/artificial_intelligence_js/05_astar.html)
 
-[Genetic Algorithm](https://luisespino.github.io/artificial_intelligence_js/06_ag.html)
+[Genetic Algorithm](https://diegomazariegos2002.github.io/artificial_intelligence_js/06_ag.html)
